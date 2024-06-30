46931bbf4588ecb72b0e8ab3392d2ee3aabdc835
DenisShaikin <35135287+DenisShaikin@users.noreply.github.com>
2024-06-30 15:05:11 +0300

    300624

--- boot.sh
+++ boot.sh
@@ -13,4 +13,4 @@ flask db upgrade
 
 #exec celery -A celery_worker.celery worker -P gevent --loglevel=INFO &
 
-exec gunicorn -b :5000 --workers 3 --threads 100 --timeout 90 --access-logfile - --error-logfile - app:run
+exec gunicorn -b :5000 --workers 3 --threads 100 --timeout 90 --access-logfile - --error-logfile - run:app
