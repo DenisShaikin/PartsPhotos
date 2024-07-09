a9a7896d594414786c827debdd1c19cd03ad62a8
DenisShaikin <35135287+DenisShaikin@users.noreply.github.com>
2024-07-09 17:34:44 +0300

    Update views.py

--- apps/api/views.py
+++ apps/api/views.py
@@ -31,32 +31,21 @@ def multibrands():
     except:
         isPreview=''
 
-
-    filepath = app.config['DOMAIN_NAME'] + '\/static\/' + brand + '\/' + article
+    # filepath = app.config['DOMAIN_NAME'] + '\/static\/' + brand + '\/' + article
     # print(filepath)
     filepath_= os.path.join(app.config['BASEDIR_'], 'apps', 'static')
-    print('filepath_=', filepath_)
+    # print('filepath_=', filepath_)
     f = []
-    myDict={}
-    df = pd.DataFrame(columns=['src', 'res'])
-    # for f in filenames:
-    # for dirpath, dirnames, filenames in os.walk(filepath_):
-    #     print(dirpath, dirnames, filenames)
-    #     # f = dirpath + '/' +  dirnames.lower().replace('/', '-') +  '/' + filenames.lower().replace('-', '').replace(' ', '')
-    # print(f)
+    # df = pd.DataFrame(columns=['src', 'res'])
     f = [dirpath+'/'+ f for (dirpath, dirnames, filenames) in os.walk(filepath_) for f in filenames]
     # print(f)
     fList = [{'src':itemf.lower().replace('-', ''), 'res':itemf} for itemf in f]
-    # fList=[f for itemf in f]
-    # print(fList)
     df = pd.DataFrame.from_dict(fList)
     # df.index=df['src']
     #Забираем список нужных путей
-    df.to_excel('test.xlsx')
-    # print(df.loc[df['src'].str.contains(article)].head())
+    # df.to_excel('test.xlsx')
     dfResult = df.loc[(df['src'].str.contains(brand.lower())) & (df['src'].str.contains(article.lower()))]['res']
-    # print(brand, article)
-    # print(dfResult.head())
+
     # собираем список
     ptotosList= []
     for item in dfResult:
@@ -68,20 +57,12 @@ def multibrands():
                 resLink = app.config['DOMAIN_NAME'] + 'static/' + brand_ + '/' + article_
                 # resLink = str(json.dumps(resLink).replace('/', r'\/'))[1:-1]
                 ptotosList.append({'url': resLink})
-        # if os.path.exists(filepath_+'.jpg'):
-        #     ptotosList.append({'url:"': filepath + ".jpg"})
-        # for i in range(10):
-        #     if os.path.exists(filepath_ + '_' + str(i) + '.jpg'):
-        #         ptotosList.append({'"url:"': filepath + '_' + str(i) + '.jpg"'})
         else:
             if ('_mini' in article_):
                 resLink = app.config['DOMAIN_NAME'] + 'static/' + brand_ + '/' + article_
-                print(resLink)
-                # resLink = ujson.dumps(resLink) #.replace('/', r'\/'))[1:-1]
+                # print(resLink)
                 ptotosList.append({'url': str(resLink)})
-        # ptotosList = json.dumps(ptotosList).replace('/', r'\/')
-        # print(json.dumps(ptotosList).replace('/', r'\/'))
-        # print(ptotosList)
+
     return (json.dumps(ptotosList).replace('/', r'\/'))
 
 @blueprint.route("/<num>", methods=["GET"])
